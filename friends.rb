3922af71e5cdc3d6d1b70a516aa9723121edcfa2
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-25 16:45:32 -0400

    Initial Commit

---
+++ friends.rb
@@ -0,0 +1,14 @@
+nums_array =[5,10,25,66,82,90,61,100]
+
+
+def above_thirty(array)
+    array.each do |number|
+        if number > 30
+            puts number 
+        end
+    end  
+end 
+
+above_thirty(nums_array)
+
+
